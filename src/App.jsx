823975464a535f01931c6d8c8d737dd2af39707e
Son Do <dominhson580@gmail.com>
2023-09-17 15:36:22 +0700

    Install react-slick

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import About from "@/components/About";
+import Projects from "@/components/Projects";
+import Contact from "@/components/Contact";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <main>
+      <About />
+      <Projects />
+      <Contact />
+    </main>
+  );
 }
 
 export default App;
