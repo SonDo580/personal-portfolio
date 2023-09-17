53617cb32a03aab146b85cb34cb7fa195ebccf38
Son Do <dominhson580@gmail.com>
2023-09-17 15:01:41 +0700

    Remove slick-carousel and vite-plugin-svgr

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.join(__dirname, "src"),
