56e2274269441ae81f031af6b07538d1e3658710
Son Do <dominhson580@gmail.com>
2023-09-17 22:40:08 +0700

    Change image names

---
+++ src/sections/Projects/images/index.js
@@ -0,0 +1,51 @@
+import adminDashboard from "./adminDashboard.png";
+import calculator from "./calculator.png";
+import crownClothing from "./crownClothing.png";
+import cvApp from "./cvApp.png";
+import githubBattle from "./githubBattle.png";
+import goalSetter from "./goalSetter.png";
+import inventoryApp from "./inventoryApp.png";
+import landingPage from "./landingPage.png";
+import library from "./library.png";
+import lotterySite from "./lotterySite.png";
+import mdnLibrary from "./mdnLibrary.png";
+import membershipSite from "./membershipSite.png";
+import memoryCards from "./memoryCards.png";
+import photoApp from "./photoApp.png";
+import restaurantPage from "./restaurantPage.png";
+import rockPaperScissors from "./rockPaperScissors.png";
+import shoppingCart from "./shoppingCart.png";
+import signupForm from "./signupForm.png";
+import sketchingPad from "./sketchingPad.png";
+import ticTacToe from "./ticTacToe.png";
+import todoList from "./todoList.png";
+import todoRedux from "./todoRedux.png";
+import weatherApp from "./weatherApp.png";
+
+const IMAGES = {
+  adminDashboard,
+  calculator,
+  crownClothing,
+  cvApp,
+  githubBattle,
+  goalSetter,
+  inventoryApp,
+  landingPage,
+  library,
+  lotterySite,
+  mdnLibrary,
+  membershipSite,
+  memoryCards,
+  photoApp,
+  restaurantPage,
+  rockPaperScissors,
+  shoppingCart,
+  signupForm,
+  sketchingPad,
+  ticTacToe,
+  todoList,
+  todoRedux,
+  weatherApp,
+};
+
+export { IMAGES };
