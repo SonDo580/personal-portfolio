c8ba7dbd0404bbe4ae3fce61fc4c571276d709a0
Son Do <dominhson580@gmail.com>
2023-09-18 11:22:00 +0700

    Render projects

--- src/sections/Projects/index.jsx
+++ src/sections/Projects/index.jsx
@@ -1,13 +1,17 @@
 import Carousel from "@/components/Carousel";
-import { reactjsProjects } from "./projects";
+import { PROJECTS } from "./projects";
 
 export default function Projects() {
   return (
     <section className="projects">
       <h1>Projects</h1>
 
-      <h2>ReactJS</h2>
-      <Carousel items={reactjsProjects} />
+      {PROJECTS.map(({ heading, items }, index) => (
+        <div key={index} className="projectsContainer">
+          <h2>{heading}</h2>
+          <Carousel items={items} />
+        </div>
+      ))}
     </section>
   );
 }
