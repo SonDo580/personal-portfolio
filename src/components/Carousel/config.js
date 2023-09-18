72a0de24e0b0fd3f8355c2c7c1b312d59c1a8823
Son Do <dominhson580@gmail.com>
2023-09-18 11:10:49 +0700

    Change slider config

--- src/components/Carousel/config.js
+++ src/components/Carousel/config.js
@@ -1,27 +1,32 @@
 export const settings = {
+  accessibility: true,
   lazyLoad: true,
+  dots: true,
   infinite: false,
   speed: 500,
   initialSlide: 0,
-  slidesToScroll: 1,
   slidesToShow: 4,
+  slidesToScroll: 4,
   responsive: [
     {
-      breakpoint: 992,
+      breakpoint: 1200,
       settings: {
         slidesToShow: 3,
+        slidesToScroll: 3,
       },
     },
     {
-      breakpoint: 768,
+      breakpoint: 820,
       settings: {
         slidesToShow: 2,
+        slidesToScroll: 2,
       },
     },
     {
       breakpoint: 576,
       settings: {
         slidesToShow: 1,
+        slidesToScroll: 1,
       },
     },
   ],
